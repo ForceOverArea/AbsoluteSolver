01a73f152fc66bdfdde72f35ef149e6eaa1162b5
Grant <christiansengrant18@gmail.com>
2025-03-24 22:35:54 -0500

    added Map-less alternative to solvedForValue

--- haskell/src/Data/AbsoluteSolver.hs
+++ haskell/src/Data/AbsoluteSolver.hs
@@ -8,6 +8,7 @@ import Data.AbsoluteSolver.Evaluate (evaluate, Context)
 import Data.AbsoluteSolver.Isolate (isolate, Steps)
 import Data.AbsoluteSolver.Parser (parseEquation)
 import Data.AbsoluteSolver.Structures (Equation(..))
+import Data.Map (fromList)
 
 solvedFor :: String -> String -> Either String (Equation, Steps)
 solvedFor eqn sym = do
@@ -20,6 +21,9 @@ solvedForValue eqn sym ctx = do
     let Equation (_, value) = eqn'
     value' <- stringizeError $ evaluate value ctx
     return (value', eqn', steps)
+
+solvedForValue' :: String -> String -> [(String, Double)] -> Either String (Double, Equation, Steps)
+solvedForValue' eqn sym ctx = solvedForValue eqn sym $ fromList ctx
 
 stringizeError :: Show a => Either a b -> Either String b
 stringizeError = left show
