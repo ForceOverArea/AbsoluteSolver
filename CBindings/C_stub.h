9a4ca285e7a3891e30b08c36e587eea5feec1241
Grant <christiansengrant18@gmail.com>
2025-04-04 18:33:07 -0500

    renaming

---
+++ CBindings/C_stub.h
@@ -0,0 +1,11 @@
+#include <HsFFI.h>
+#if defined(__cplusplus)
+extern "C" {
+#endif
+extern void free(HsPtr a1);
+extern HsPtr solvedForHs(HsPtr a1, HsPtr a2);
+extern HsDouble solvedForValueHs(HsPtr a1, HsPtr a2, HsPtr a3);
+#if defined(__cplusplus)
+}
+#endif
+
